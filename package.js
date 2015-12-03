1c9aa0b8592d1c7275d967044a6262173da0567a
Valentin Funk <funk.valentin@gmail.com>
2015-12-03 23:58:50 +0000

    Fix add_files to addAssets

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
 	summary: "A paypal package that helps make restful API calls to Paypal",
 	version: "1.0.1",
-	name: "limemakers:paypal",
-	git: "https://github.com/LimeMakers/meteor-paypal.git"
+	name: "kamshak:paypal",
+	git: "https://github.com/Kamshak/meteor-paypal.git"
 });
 
 Npm.depends({
@@ -11,7 +11,7 @@ Npm.depends({
 
 Package.on_use(function(api) {
 	api.add_files("paypal.js", ["client", "server"]);
-	api.add_files(["paypal_credit_card_form.html", "paypal_credit_card_form.js"], "client");
+	api.addAssets(["paypal_credit_card_form.html", "paypal_credit_card_form.js"], "client");
 	api.export("Paypal", ["client", "server"]);
 });
 
